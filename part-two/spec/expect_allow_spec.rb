73eb5bac781f94660a86d937b6f8d69ab0ae9228
natashabuckham <natashaisobel@gmail.com>
2023-03-22 14:53:24 +0000

    part two exercise complete

---
+++ part-two/spec/expect_allow_spec.rb
@@ -0,0 +1,30 @@
+
+
+# As a double
+RSpec.describe "example" do
+  it "example 1" do
+    # This creates the double
+    fake_object = double(:fake_object)
+    # This creates a 'stub' (fake method) on the double
+    allow(fake_object).to receive(:greet).with("Kay").and_return("Hello, Kay!")
+
+    # This is just to show you what it can now do
+    expect(fake_object.greet("Kay")).to eq "Hello, Kay!"
+    # fake_object.greet("Henry") # This will throw an error
+  end
+end
+
+RSpec.describe "example" do
+  it "example 2" do
+    # This creates the double
+    fake_object = double(:fake_object)
+    # This creates a 'stub' (fake method) on the double
+    # But using 'expect' means the test will fail unless it is called.
+    expect(fake_object).to receive(:greet).with("Kay").and_return("Hello, Kay!")
+
+    # This is just to show you what it can now do
+    expect(fake_object.greet("Kay")).to eq "Hello, Kay!"
+    # fake_object.greet("Henry") # This will throw an error
+    # If you comment out the above two lines, the test will fail.
+  end
+end
